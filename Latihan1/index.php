eb917251ef99008ce5ccf83a4269a42836830fa5
awataravirsa <79452353+awataravirsa@users.noreply.github.com>
2021-02-26 11:45:31 +0800

    Add files via upload

---
+++ Latihan1/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Selamat Datang di Website Drama Korea Terbaru</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <link rel="canonical" href="http://informatika.undiksha.ac.id/">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body class ="mt-5">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div>
+    <a class="navbar-brand" href="#">DRAKORINDO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+     aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" href="#">Home<span class="sr-only":>(current)</span></a>
+        <a class="nav-item nav-link" href="#">Drama Korea</a>
+        <a class="nav-item nav-link" href="#">Film Korea</a>
+        <a class="nav-item nav-link" href="#">Variety Show</a>
+      </div>
+    </div>
+    </div>
+</nav>
+    <div class="jumbotron text-center">
+        <h1>DRAMA KOREA UPDATE</h1>
+        <p>Drama Korea and Variety Show</p>
+        <img src="media\dramakorea.jpg">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <center><h3>ROMANCE</h3></center>
+                <p>Descendants of the Sun</p>
+                <p>The Heirs</p>
+                <p>Boys Over Flowers</p>
+            </div>
+            <div class="col-sm-4">
+                <center><h3>COMEDY</h3></center>
+                <p>Fight My Way</p>
+                <p>Welcome to Waikiki</p>
+                <p>Rootop Prince</p>
+            </div>
+            <div class="col-sm-4">
+                <center><h3>ACTION</h3></center>
+                <p>Vagabond</p>
+                <p>Voice</p>
+                <p>Signal</p>
+            </div>
+            <div class="col-sm-4">
+                <center><h3>MELODRAMA</h3></center>
+                <p>Moon Lovers: Scarlet Heart Ryeo</p>
+                <p>Uncontrollably Fond</p>
+                <p>Guardian: The Lonely and Great God</p>
+            </div>
+            <div class="col-sm-4">
+                <center><h3>MOVIE</h3></center>
+                <p>Secretly, Greatly</p>
+                <p>My Annoying Brother</p>
+                <p>Alive</p>
+            </div>
+            <div class="col-sm-4">
+                <center><h3>VARIETY SHOW</h3></center>
+                <p>Running Man</p>
+                <p>Knowing Brother</p>
+                <p>Happy Together</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
